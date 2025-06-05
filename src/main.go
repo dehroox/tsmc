0ef4ca7cd2067a746d67f7c220d20e23c72a96ec
dehrexd <dehrexd@gmail.com>
2025-06-05 11:35:05 +0800

    Basic File Reading and Input

--- src/main.go
+++ src/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	var location string
+
+	fmt.Print("What file(s) should be compiled? ")
+	fmt.Scanln(&location)
+	fmt.Println("Got it! Compiling now... ")
+
+	file, err := os.Open(location)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
